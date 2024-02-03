aeb63a935cdf063d96300be56c18950bb0d74a2e
Alessandro Andrade <andrade.sp27@gmail.com>
2024-02-03 16:06:06 -0400

    Atualização do repositório

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,39 @@
+//hooks
+    //before - antes de todos os testes
+    //beforeEach - antes de cada teste
+    //after - depois de todos os testes
+    //afterEach - depois de cada teste
+
+/// <reference types="cypress" />
+const element = require("../fixtures/login.json")
+
+beforeEach(() => {
+    cy.visit('https://automacao.qacoders-academy.com.br/login');
+});
+
+afterEach(() => {
+    cy.screenshot();
+})
+
+
+describe('Login', () => {
+    it('Login com sucesso', () => {
+        const email = Cypress.env('EMAIL');
+        const password = Cypress.env('PASSWORD');
+        cy.Login(email, password);
+    });
+
+    it('Login com e-mail válido e a senha inválida', () => {
+        cy.get(element.input_email).type(Cypress.env('EMAIL'));
+        cy.get(element.input_password).type(Cypress.env('PASSWORD_INVALIDO'));
+        cy.get(element.btn_login).click().screenshot('bt_entrar');
+        cy.MsgLoginFalho()
+    });
+
+    it('Login com e-mail inválido e a senha válida', () => {
+        cy.get(element.input_email).type(Cypress.env('EMAIL_INVALIDO'));
+        cy.get(element.input_password).type(Cypress.env('PASSWORD'));
+        cy.get(element.btn_login).click();
+        cy.MsgLoginFalho()
+    });
+});
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,3 +1,23 @@
+/// <reference types="cypress" />
+
+Cypress.Commands.add('Login', (email, password) => {
+    cy.get('#email').type(email);
+    cy.get('#password').type(password);
+    // cy.contains('button', 'ENTRAR').click();
+    cy.get('#login').click();
+
+    cy.location().should((loc) => {
+        expect(loc.pathname).to.eql('/login');
+    })
+
+    cy.get('body > nav > button').should('be.visible');
+});
+
+Cypress.Commands.add('MsgLoginFalho', () => {
+    cy.get('[class="MuiAlert-message css-1xsto0d"]').should('be.visible');
+    cy.get('[class="MuiAlert-message css-1xsto0d"]').should('have.text', 'E-mail e/ou senha inválidos');
+});
+
 // ***********************************************
 // This example commands.js shows you how to
 // create various custom commands and overwrite
